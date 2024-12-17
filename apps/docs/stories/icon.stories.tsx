ebab930a0d54455b0f618ae72e638a74fc61a372
Nate Lubeck <me@natelubeck.com>
2024-12-17 06:35:24 -0800

    feat(icons): updated icons from figma

--- apps/docs/stories/icon.stories.tsx
+++ apps/docs/stories/icon.stories.tsx
@@ -1,5 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import OffRoadIcon from "@murrieta/ui/foundation/icons";
+import OffRoadIcon, {NLLogo,
+  BriefcaseIcon, ChartIcon,
+  ClipboardIcon, CodeIcon, HandshakeIcon,
+  GraffitiIcon, PaintingIcon, CityIcon,
+  WarehouseIcon, CarBuildingIcon, BuildingIcon,
+  BootIcon, BackpackIcon, CampfireIcon, CompassIcon } from "@murrieta/ui/foundation/icons";
 
 const meta: Meta<typeof OffRoadIcon> = {
   component: OffRoadIcon,
@@ -15,6 +20,84 @@ type Story = StoryObj<typeof OffRoadIcon>;
  * to learn how to use render functions.
  */
 export const Primary: Story = {
-  render: ()=> (<OffRoadIcon /> ),
+  render: ()=> (<OffRoadIcon size="lg"/> ),
   name: "OffRoadIcon"
 };
+
+export const NLLogoIcon: Story = {
+  render: () => (<NLLogo size="xl" />),
+  name: "NLLogo",
+};
+
+export const Briefcase: Story = {
+  render: () => (<BriefcaseIcon />),
+  name: "BriefcaseIcon",
+};
+
+export const Chart: Story = {
+  render: () => <ChartIcon />,
+  name: "ChartIcon",
+};
+
+export const Clipboard: Story = {
+  render: () => <ClipboardIcon />,
+  name: "ClipboardIcon",
+};
+
+export const Code: Story = {
+		render: () => <CodeIcon />,
+		name: "CodeIcon",
+};
+
+export const Handshake: Story = {
+		render: () => <HandshakeIcon />,
+		name: "HandshakeIcon",
+};
+
+export const Graffiti: Story = {
+  render: () => <GraffitiIcon />,
+  name: "GraffitiIcon"
+}
+
+export const Painting: Story = {
+		render: () => <PaintingIcon />,
+		name: "PaintingIcon",
+};
+
+export const City: Story = {
+  render: () => <CityIcon />,
+  name:"CityIcon"
+}
+export const Warehouse: Story = {
+  render: () => <WarehouseIcon />,
+  name:"WarehouseIcon"
+}
+
+export const CarBuilding: Story = {
+  render: () => <CarBuildingIcon />,
+  name:"CarBuildingIcon"
+}
+export const Building: Story = {
+  render: () => <BuildingIcon />,
+  name:"BuildingIcon"
+}
+
+export const Boot: Story = {
+  render: () => <BootIcon />,
+  name: "BootIcon",
+};
+
+export const Backpack: Story = {
+  render: () => <BackpackIcon />,
+  name:"BackpackIcon"
+}
+
+export const Campfire: Story = {
+  render: () => <CampfireIcon />,
+  name:"CampfireIcon",
+};
+
+export const Compass: Story = {
+  render: () => <CompassIcon />,
+  name:"CompassIcon"
+}
--- packages/ui/src/foundation/icons.tsx
+++ packages/ui/src/foundation/icons.tsx
@@ -3,30 +3,30 @@ import { byPrefixAndName } from '@awesome.me/kit-a2bfc4dc7b/icons'
 
 
 // Logo
-
+export const NLLogo = (props:any) => <FontAwesomeIcon icon={byPrefixAndName.fak['nl-initials-logo']} {...props} />
 // Professional - solid - fas : briefcase, chart-fft, clipboard-list, file-code, handshake
-export const BriefcaseIcon = <FontAwesomeIcon icon={byPrefixAndName.fas['briefcase']} />
-export const ChartIcon = <FontAwesomeIcon icon={byPrefixAndName.fas['chart-fft']} />
-export const ClipboardIcon = <FontAwesomeIcon icon={byPrefixAndName.fas['clipboard-list']} />
-export const CodeIcon = <FontAwesomeIcon icon={byPrefixAndName.fas['file-code']} />
-export const HandshakeIcon = <FontAwesomeIcon icon={byPrefixAndName.fas['handshake']} />
+export const BriefcaseIcon = (props:any) => <FontAwesomeIcon icon={byPrefixAndName.fasds.briefcase} {...props} />
+export const ChartIcon = (props:any) => <FontAwesomeIcon icon={byPrefixAndName.fasds['chart-fft']} {...props} />
+export const ClipboardIcon = (props:any) => <FontAwesomeIcon icon={byPrefixAndName.fasds['clipboard-list']} {...props} />
+export const CodeIcon = (props: any) => <FontAwesomeIcon icon={byPrefixAndName.fasds['file-code']} {...props} />
+export const HandshakeIcon = (props:any) => <FontAwesomeIcon icon={byPrefixAndName.fasds.handshake} {...props} />
 
 
 // Personal - duotone - fasds: spray-can, paintbrush-pencil, city, warehouse-full, car-building, building
-export const GraffitiIcon = <FontAwesomeIcon icon={byPrefixAndName.fasds['spray-can']} />
-export const PaintingIcon = <FontAwesomeIcon icon={byPrefixAndName.fasds['paintbrush-pencil']} />
-export const CityIcon = <FontAwesomeIcon icon={byPrefixAndName.fasds['city']} />
-const WarehouseIcon = <FontAwesomeIcon icon={byPrefixAndName.fasds['warehouse-full']} />
-export const CarBuildingIcon = <FontAwesomeIcon icon={byPrefixAndName.fasds['car-building']} />
-export const BuildingIcon = <FontAwesomeIcon icon={byPrefixAndName.fasds['building']} />
+export const GraffitiIcon = (props: any) => <FontAwesomeIcon icon={byPrefixAndName.fasds['spray-can']} {...props} />
+export const PaintingIcon = (props: any) => <FontAwesomeIcon icon={byPrefixAndName.fasds['paintbrush-pencil']} {...props} />
+export const CityIcon = (props:any) => <FontAwesomeIcon icon={byPrefixAndName.fasds.city} {...props} />
+export const WarehouseIcon = (props:any) => <FontAwesomeIcon icon={byPrefixAndName.fasds['warehouse-full']} {...props} />
+export const CarBuildingIcon = (props:any) => <FontAwesomeIcon icon={byPrefixAndName.fasds['car-building']} {...props} />
+export const BuildingIcon = (props:any) => <FontAwesomeIcon icon={byPrefixAndName.fasds.building} {...props} />
 
 
 // Outdoors - solid fas: truck-monster, boot, backpack, campfire, compass
-export const OffRoadIcon = <FontAwesomeIcon icon={byPrefixAndName.fas['truck-monster']} />
-export const BootIcon = <FontAwesomeIcon icon={byPrefixAndName.fas['boot']} />
-export const BackpackIcon = <FontAwesomeIcon icon={byPrefixAndName.fas['backpack']} />
-export const CampfireIcon = <FontAwesomeIcon icon={byPrefixAndName.fas['campfire']} />
-export const CompassIcon = <FontAwesomeIcon icon={byPrefixAndName.fas['compass']} />
+export const OffRoadIcon = (props: any) => <FontAwesomeIcon icon={byPrefixAndName.fasds["truck-monster"]} {...props} />
+export const BootIcon = (props: any) => <FontAwesomeIcon icon={byPrefixAndName.fasds.boot} {...props} />
+export const BackpackIcon = (props: any) => <FontAwesomeIcon icon={byPrefixAndName.fasds.backpack} {...props} />
+export const CampfireIcon = (props: any) => <FontAwesomeIcon icon={byPrefixAndName.fasds.campfire} {...props} />
+export const CompassIcon = (props: any) => <FontAwesomeIcon icon={byPrefixAndName.fasds.compass} {...props} />
 
 export default OffRoadIcon
 
